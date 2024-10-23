a0c5a0e0adf7230d1df6cb95c4c2488a15c6bea8
Jack <bjackrobert@gmail.com>
2024-10-22 20:44:36 -0400

    ArmTester Added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/ArmTester.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.Teleops;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.SubSystems.subShoulder;
+
+@TeleOp(name="Arm Test")
+public class ArmTester extends LinearOpMode {
+
+    subShoulder should = new subShoulder(hardwareMap);
+    int extendDist = 0;
+    int rotateDist = 0;
+
+    double defShPow = 0.5;
+
+    @Override
+    public void runOpMode() {
+        if (Math.abs(gamepad1.right_stick_y) > .05) {
+            should.setShld((int)(should.slides.getCurrentPosition() + 50 * gamepad1.right_stick_y), defShPow);
+        }
+
+        if (Math.abs(gamepad1.left_stick_y) > .05) {
+            should.setShld((int)(should.shoulder.getCurrentPosition() + 50 * gamepad1.left_stick_y), defShPow);
+        }
+
+        telemetry.addData("Slide Value: ", should.slides.getCurrentPosition());
+        telemetry.addData("Shoulder Value: ", should.shoulder.getCurrentPosition());
+    }
+}
