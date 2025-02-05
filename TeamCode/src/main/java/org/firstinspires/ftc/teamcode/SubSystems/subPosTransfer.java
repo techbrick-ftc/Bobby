62364e927c542bfaafe5dcb8816a142e9e32c99e
Jack <bjackrobert@gmail.com>
2025-02-04 19:00:22 -0500

    Added pos class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/subPosTransfer.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.SubSystems;
+
+public class subPosTransfer {
+    static double ang;
+
+
+    public double getAngle(){
+        return ang;
+    }
+
+    public void setAngle(double angle){
+        ang = angle;
+    }
+}
